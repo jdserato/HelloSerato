129858235c6fedf40422061908e8fe485c95847a
Jay Vince Serato <jayvince.serato@gmail.com>
2022-07-01 15:59:45 +0800

    Add a button

--- app/src/main/java/com/example/helloserato/MainActivity.java
+++ app/src/main/java/com/example/helloserato/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.helloserato;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonOnClickListener();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    public void buttonOnClickListener() {
+        Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("i", "Hello");
+            }
+        });
     }
 }
